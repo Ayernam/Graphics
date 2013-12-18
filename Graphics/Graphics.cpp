d1f90acfb412e5674917e1c14ff2f52de3b04943
Amir Amhaz <namhaza4@outlook.com>
2013-12-17 21:09:51 -0500

    Minor changes

--- Graphics/Graphics.cpp
+++ Graphics/Graphics.cpp
@@ -5,35 +5,35 @@
 #include<string>
 using namespace std;
 int rand_0toN1(int n);
-char *g[11][11];
-int x=5,y=5;
+char *g[20][50];
+int x=25,y=10, ex, ey;
 string input;
 int main() {
     cout << "Welcome to the game!  Press the WASD keys and ENTER to move.  Press 0 to exit." << endl;
     system("PAUSE");
     while(1==1){
         system("CLS");
-        for(int i=0;i<11;i++){
-            for(int j=0;j<11;j++){
+        for(int i=0;i<20;i++){
+            for(int j=0;j<50;j++){
                 g[i][j]=" ";
                 g[y][x]=">";
                 cout << g[i][j];
-                if(j==10){
+                if(j==49){
                     cout << endl;
                 }
             }
         }
         cin >> input;
-        if(input=="W"){
+        if(input=="W" && y>0){
             y-=1;
         }
-        if(input=="A"){
+        if(input=="A" && x>0){
             x-=1;
         }
-        if(input=="S"){
+        if(input=="S" && y<20){
             y+=1;
         }
-        if(input=="D"){
+        if(input=="D" && x<50){
             x+=1;
         }
         if(input=="0"){
